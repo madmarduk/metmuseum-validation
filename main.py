5d5d312c433e12a31c1bd40a7d1576e02a0517e1
Mad Marduk <madmarduk@yandex.ru>
2024-05-24 14:14:36 +0300

    Edit gitignore

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
     # TODO: Работа фильтрации и сортировки результатов поиска (если предусмотрено API)
 
 import requests
+import logging
 from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional, List
